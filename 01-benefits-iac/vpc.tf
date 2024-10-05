d63b05e546637316636c8d3d507b5881c62a7f77
picklu <p.ciklu@gmail.com>
2024-10-05 17:35:45 +0530

    added public and private resources

--- 01-benefits-iac/vpc.tf
+++ 01-benefits-iac/vpc.tf
@@ -18,3 +18,13 @@ resource "aws_vpc" "demo_vpc" {
     Name = "Terraform VPC"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
