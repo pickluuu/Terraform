b556d2fc294560901df46bee84ec3a183313ad92
picklu <p.ciklu@gmail.com>
2024-10-08 00:38:00 +0530

    updated the value to check the syntax and process to input the value

--- hcl-02/hcl.tf
+++ hcl-02/hcl.tf
@@ -10,3 +10,25 @@ terraform {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my_bucket"
 }
+
+data "aws_s3_bucket" "my_external_bucket" {
+  bucket = "not-managed-by-us"
+}
+
+variable "my_bucket" {
+  type        = string
+  description = "My variable used to set bucket name"
+  default     = "my_default_bucket_name"
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.my_bucket.id
+}
+
+locals {
+  local_example = "This is a local variable"
+}
+
+module "my_module" {
+  source = "./module-example"
+}
