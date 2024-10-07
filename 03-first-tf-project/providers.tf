e62047cddfc0fc54d4077a33987ef09f2cedc1a5
picklu <p.ciklu@gmail.com>
2024-10-08 00:55:51 +0530

    created the first tf project

--- 03-first-tf-project/providers.tf
+++ 03-first-tf-project/providers.tf
@@ -14,3 +14,15 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+resource "random_id" "bucket_siffix" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "bucket_firt_tf_project" {
+  bucket = "my-tf-bucket-${random_id.bucket_siffix.hex}"
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.bucket_firt_tf_project.id
+}
