f3925601ca123c1e8daf48d5c7329c6b451d418d
picklu <p.ciklu@gmail.com>
2024-10-08 23:48:54 +0530

    update the subnet cidr block

--- 06-resources/networking.tf
+++ 06-resources/networking.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = "aws_vpc.this.id"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
 
 }
 
