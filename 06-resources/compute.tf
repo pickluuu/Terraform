03dc15afc58dbffed93c11d7c0346566bec62457
picklu <p.ciklu@gmail.com>
2024-10-09 00:48:31 +0530

    update an instance  type

--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                         = "ami-00f117fe174f83c56"
   associate_public_ip_address = true
-  instance_type               = "t2.micro"
+  instance_type               = "t4g.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   root_block_device {
     delete_on_termination = true
