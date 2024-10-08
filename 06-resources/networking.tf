74fe5361e4462404463e708c2e45037b22bb880d
picklu <p.ciklu@gmail.com>
2024-10-08 23:53:37 +0530

    update the syntax from literal string to dynamically status

--- 06-resources/networking.tf
+++ 06-resources/networking.tf
@@ -11,18 +11,18 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = "aws_vpc.this.id"
+  vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = "aws_vpc.this.id"
+  vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.2.0/24"
 
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "aws_vpc.this.id"
+  vpc_id = aws_vpc.this.id
 
   tags = {
     Name = "internet-gateway"
@@ -30,7 +30,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route_table" "public_route" {
-  vpc_id = "aws_vpc.this.id"
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
