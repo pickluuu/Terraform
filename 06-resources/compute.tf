77f62faddd9be2d1640116239bfde4ad8868c6fa
picklu <p.ciklu@gmail.com>
2024-10-13 17:10:26 +0530

    update the security group syntax

--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "public_http_traffic" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "http" {
-  security_group_id = aws_security_group.public_http_traffic
+  security_group_id = aws_security_group.public_http_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
