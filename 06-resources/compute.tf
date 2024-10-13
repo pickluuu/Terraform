97bd4575891fb46209d3895a1af4e396c9f2b3d9
picklu <p.ciklu@gmail.com>
2024-10-13 17:02:49 +0530

    update the security group and attached into the ec2 instances

--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -1,11 +1,27 @@
-# resource "aws_instance" "web" {
-#   ami                         = "ami-00f117fe174f83c56"
-#   associate_public_ip_address = true
-#   instance_type               = "t4g.micro"
-#   subnet_id                   = aws_subnet.public_subnet.id
-#   root_block_device {
-#     delete_on_termination = true
-#     volume_size           = 10
-#     volume_type           = "gp3"
-#   }
-# }
+resource "aws_instance" "web" {
+  ami                         = "ami-00f117fe174f83c56"
+  associate_public_ip_address = true
+  instance_type               = "t4g.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group_ingress_rule.public_http_traffic]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "allow_tls"
+  vpc_id      = aws_vpc.this.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
