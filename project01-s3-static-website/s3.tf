797740eeb2b3dab8d1429c76b03e32317bd5cae8
picklu <p.ciklu@gmail.com>
2024-10-14 19:22:50 +0530

    updated the public access block in s3 file

--- project01-s3-static-website/s3.tf
+++ project01-s3-static-website/s3.tf
@@ -5,3 +5,12 @@ resource "random_id" "bucket_suffix" {
 resource "aws_s3_bucket" "static_website" {
   bucket = "terraform-course-project-1-${random_id.bucket_suffix.hex}"
 }
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
