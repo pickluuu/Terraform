ed35120e88e3ff041a0fb233eb6774b2faa93ece
picklu <p.ciklu@gmail.com>
2024-10-14 20:32:16 +0530

    added and update an index.html and error.html

--- project01-s3-static-website/s3.tf
+++ project01-s3-static-website/s3.tf
@@ -31,4 +31,17 @@ resource "aws_s3_bucket_policy" "static_website" {
       }
     ]
   })
-}  
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
