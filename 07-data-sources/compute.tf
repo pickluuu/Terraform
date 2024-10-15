a79126b344d173c5a6e77a09cd5f96b77218d27c
picklu <p.ciklu@gmail.com>
2024-10-15 22:35:33 +0530

    updated caller identity

--- 07-data-sources/compute.tf
+++ 07-data-sources/compute.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
 # data "aws_region" "current" {}
 
@@ -24,9 +24,9 @@ output "ubuntu_ami_data" {
   value = data.aws_ami.ubuntu
 }
 
-# output "aws_caller_identity" {
-#   value = data.aws_caller_identity.ubuntu.id
-# }
+output "aws_caller_identity" {
+  value = data.aws_caller_identity.ubuntu.id
+}
 
 # output "aws_region" {
 #   value = data.aws_region.ubuntu.id
