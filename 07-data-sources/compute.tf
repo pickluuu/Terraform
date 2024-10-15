471982a56a1a7ecc6437a211b7e3b034e5a06157
picklu <p.ciklu@gmail.com>
2024-10-15 18:13:44 +0530

    Impoprted the data block with (aws_ami) ubuntu

--- 07-data-sources/compute.tf
+++ 07-data-sources/compute.tf
@@ -1,11 +1,32 @@
-resource "aws_instance" "web" {
-  ami                         = "ami-07b783cb49fc7ef94"
-  associate_public_ip_address = true
-  instance_type               = "t3a.small"
+data "aws_ami" "ubuntu" {
 
-  root_block_device {
-    delete_on_termination = true
-    volume_size           = 10
-    volume_type           = "gp3"
+  most_recent = true
+  owners      = ["099720109477"] # Owner is canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
+
+output "test" {
+  value = data.aws_ami.ubuntu
+}
+
+
+# resource "aws_instance" "web" {
+#   ami                         = "ami-07b783cb49fc7ef94"
+#   associate_public_ip_address = true
+#   instance_type               = "t3a.small"
+
+#   root_block_device {
+#     delete_on_termination = true
+#     volume_size           = 10
+#     volume_type           = "gp3"
+#   }
+# }
